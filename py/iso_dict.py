65e6f44b75c3f944bdd7e1e3802c62a17e060c7f
Nikhil S <nikhil.sonnad@gmail.com>
2013-12-15 20:18:36 +1100

    DRYing iso_dict.py

--- py/iso_dict.py
+++ py/iso_dict.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-import sys
-import re
 import csv
 
 # -------------------------------------------------------------------
@@ -8,29 +6,28 @@ import csv
 # -------------------------------------------------------------------
 
 def make_iso_dict(iso3, iso2, iso5, retired):
-    # go from old to new so that newest values win out
     retired_tabin = csv.reader(retired, dialect=csv.excel_tab)
     iso5_tabin = csv.reader(iso5, dialect=csv.excel_tab)
     iso2_tabin = csv.reader(iso2, delimiter='|')
     iso3_tabin = csv.reader(iso3, dialect=csv.excel_tab)
     iso_dict = {}
 
+    def add_entry(row, i_id, i_name):
+        id = row[i_id]
+        iso_dict[id] = row[i_name]
+
+    # go from old to new iso codes so that newest values win out
     for row in retired_tabin:
-        id = row[0]
-        iso_dict[id] = row[1]
+        add_entry(row, 0, 1)
 
     for row in iso5_tabin:
-        id = row[1]
-        iso_dict[id] = row[2]
+        add_entry(row, 1, 2)
 
     for row in iso2_tabin:
-        id = row[0]
-        iso_dict[id] = row[3]
+        add_entry(row, 0, 3)
 
     for row in iso3_tabin:
-        id = row[0]
-        name = row[6]
-        iso_dict[id] = name
+        add_entry(row, 0, 6)
         
     return iso_dict
 
